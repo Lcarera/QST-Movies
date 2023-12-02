a9a1c3de706416cdac9b5d38c07513d99826f2bc
lcarera <lcarera04@gmail.com>
2023-12-02 17:55:32 -0300

    styling movies

--- src/app/components/movies/movies.module.ts
+++ src/app/components/movies/movies.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './movies.component';
 import { MovieThumbnailComponent } from './movie-thumbnail/movie-thumbnail.component';
 import { MoviesRoutingModule } from './movies-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MoviesComponent, MovieThumbnailComponent],
-  imports: [CommonModule, MoviesRoutingModule],
+  imports: [CommonModule, MoviesRoutingModule, MatCardModule, MatButtonModule],
 })
 export class MoviesModule {}
