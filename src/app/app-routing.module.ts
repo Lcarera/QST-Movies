d03fe1c2ab459edce119841dc4c0cd7d2ec25042
lcarera <lcarera04@gmail.com>
2023-12-06 01:05:19 -0300

    Remove movie detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'movies', loadChildren: () => import('@/components/movies/movies.module').then(m => m.MoviesModule) },
-  { path: 'movie/:id', loadChildren: () => import('@/components/movies/movie-detail/movie-detail.module').then(m => m.MovieDetailModule) },
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
 ];
 
--- src/app/components/movies/movies-routing.module.ts
+++ src/app/components/movies/movies-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
+  { path: 'details/:id', component: MovieDetailComponent },
   { path: '', component: MoviesComponent },
 ];
 
