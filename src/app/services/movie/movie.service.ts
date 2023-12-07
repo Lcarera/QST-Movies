79c2ec33c29bd81c31c9b4fe0a6b2dc2bbf227eb
lcarera <lcarera04@gmail.com>
2023-12-06 23:44:23 -0300

    Refactor movie service to handle errors and use local data

--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -1,19 +1,30 @@
 import { Movie } from '@/interface/movie.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-  private http:HttpClient = inject(HttpClient);
+  private http: HttpClient = inject(HttpClient);
 
   getMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>('http://localhost:3000/movies');
+    return this.http.get<Movie[]>('assets/data/movies.json');
   }
 
   getMovie(id: string): Observable<Movie> {
-    return this.http.get<Movie>(`http://localhost:3000/movies/${id}`);
+    return this.http.get<Movie[]>('assets/data/movies.json')
+      .pipe(
+        map(movies => {
+          const movie = movies.find(movie => movie.id === Number(id));
+          if (!movie) {
+            throw new Error('Movie not found');
+          }
+          return movie;
+        }),
+        catchError(error => throwError(() => error))
+      );
   }
 }
